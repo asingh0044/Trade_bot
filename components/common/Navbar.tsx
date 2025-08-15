705a0fad1f503d0b693964edaf97c15aeb683a0d
Rayel3gant <helloakash04@gmail.com>
2025-08-15 08:34:09 +0530

    chore: add disconnect dropdown

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -1,13 +1,20 @@
 "use client";
 import { trimAddress } from "@/lib/utils";
-import { Copy } from "lucide-react";
+import { ChevronDown, Copy, LogOut } from "lucide-react";
 import Link from "next/link";
 import React from "react";
-import { useAccount, useConnect } from "wagmi";
+import { useAccount, useConnect, useDisconnect } from "wagmi";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
 
 export const Navbar = () => {
   const { connectors, connect } = useConnect();
   const { address, isConnected } = useAccount();
+  const { disconnect } = useDisconnect();
   const connectHandler = () => {
     const metamaskConnector = connectors.find(
       (connector) =>
@@ -33,7 +40,7 @@ export const Navbar = () => {
 
         <div className="flex w-fit items-center gap-x-5">
           <Link href="" className="text-white uppercase">
-            <div>Send</div>
+            <div className="text-sm">Send</div>
           </Link>
           {!isConnected ? (
             <button
@@ -43,13 +50,25 @@ export const Navbar = () => {
               Connect
             </button>
           ) : (
-            <div className="flex w-fit gap-x-2">
+            <div className="flex items-center w-fit gap-x-2">
               <div className="text-white text-sm font-semibold">
                 {trimAddress(address)}
               </div>
               <button onClick={copyToClipboard} className="text-white">
                 <Copy size={12} className="cursor-pointer " />
               </button>
+              <DropdownMenu>
+                <DropdownMenuTrigger className="outline-none border-0">
+                  <ChevronDown className="text-white" />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuItem>
+                    <button className="cursor-pointer flex items-center gap-x-3" onClick={()=>disconnect()}>
+                      <span><LogOut  size={10}/></span> Disconnect
+                    </button>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
             </div>
           )}
         </div>
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,11 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export const trimAddress = (addr?: string) => {
-    if (!addr) return "";
-    return `${addr.slice(0, 6)}...${addr.slice(-4)}`;
-  };
+  if (!addr) return "";
+  return `${addr.slice(0, 6)}...${addr.slice(-4)}`;
+};
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
