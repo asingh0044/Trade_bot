91c05e8862f885ba0c71b81c1c20815f8daf5488
Rayel3gant <helloakash04@gmail.com>
2025-08-17 08:00:16 +0530

    chore: add bg gradient

--- components/ui/background-beams-with-collision.tsx
+++ components/ui/background-beams-with-collision.tsx
@@ -70,7 +70,7 @@ export const BackgroundBeamsWithCollision = ({
     <div
       ref={parentRef}
       className={cn(
-        "h-screen bg-gradient-to-b from-neutral-950 to-neutral-800 relative overflow-hidden",
+        "h-screen relative overflow-hidden",
         className
       )}
     >
